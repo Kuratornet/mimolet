9c50a94123cb1998d0eb9bd5ff4c85bd6a643bcd
yshylov <shilov.yuri@gmail.com>
2013-06-08 14:57:31 +0300

    Add droidtext library.

--- Mimolet/gen/com/mimolet/android/R.java
+++ Mimolet/gen/com/mimolet/android/R.java
@@ -952,7 +952,8 @@ containing a value of this type.
         public static final int abs__textButton=0x7f040013;
         public static final int abs__title=0x7f04001b;
         public static final int abs__up=0x7f04000b;
-        public static final int action_settings=0x7f04003d;
+        public static final int action_settings=0x7f04003e;
+        public static final int buttonCreatePdf=0x7f04003b;
         public static final int buttonLoadPicture=0x7f04003a;
         public static final int disableHome=0x7f040009;
         public static final int edit_query=0x7f040026;
@@ -960,7 +961,7 @@ containing a value of this type.
         public static final int homeAsUp=0x7f040006;
         public static final int imgView=0x7f040039;
         public static final int listMode=0x7f040002;
-        public static final int listview=0x7f04003b;
+        public static final int listview=0x7f04003c;
         public static final int loginField=0x7f040035;
         public static final int normal=0x7f040001;
         public static final int passwordField=0x7f040034;
@@ -972,7 +973,7 @@ containing a value of this type.
         public static final int tempButton1=0x7f040038;
         public static final int textView1=0x7f040033;
         public static final int textView2=0x7f040036;
-        public static final int thumbImage=0x7f04003c;
+        public static final int thumbImage=0x7f04003d;
         public static final int useLogo=0x7f040004;
         public static final int wrap_content=0x7f040000;
     }
