aaad16a28403987eca2156927ebdc83ef4899274
yshylov <shilov.yuri@gmail.com>
2013-06-21 00:29:21 +0300

    Rewrite authorization with HTTP client.

--- Mimolet/src/com/mimolet/android/task/AuthorizationTask.java
+++ Mimolet/src/com/mimolet/android/task/AuthorizationTask.java
@@ -1,55 +1,72 @@
 package com.mimolet.android.task;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.os.AsyncTask;
 import android.util.Log;
 
-public class AuthorizationTask extends AsyncTask<String, Void, String> {
+import com.mimolet.android.AuthorizationActivity;
+import com.mimolet.android.util.Registry;
+
+public class AuthorizationTask extends
+		AsyncTask<String, Void, AuthorizationTask.ExecutionResult> {
 	private static final String TAG = "AuthorizationTask";
+
+	public enum ExecutionResult {
+		SUCCESS, FAIL
+	}
+
+	private AuthorizationActivity parent;
+
+	public AuthorizationTask(AuthorizationActivity parent) {
+		this.parent = parent;
+	}
 
 	/**
 	 * Accepts login, password, url
 	 */
 	@Override
-	protected String doInBackground(final String... params) {
+	protected ExecutionResult doInBackground(final String... params) {
 		try {
-			final String encodedData = "j_username=" + URLEncoder.encode(params[0], "UTF-8") +
-					"&j_password=" + URLEncoder.encode(params[1], "UTF-8");
-			//prepare header of request
-			final HttpURLConnection connection = (HttpURLConnection) new URL(
-					params[2]).openConnection();
-			connection.setRequestMethod("POST");
-			connection.setUseCaches(false);
-			connection.setDoInput(true);
-			connection.setDoOutput(true);
-			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			connection.addRequestProperty("Content-Length", Integer.toString(encodedData.getBytes().length));
-			connection.setRequestProperty("Content-Language", "en-US");
-			//write request data
-			final DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-    	    wr.writeBytes(encodedData);
-    	    wr.flush();
-    	    wr.close();
-    	    //read response
-			final BufferedReader br = new BufferedReader(new InputStreamReader(
-					connection.getInputStream()));
-			final StringBuilder response = new StringBuilder();
-			String inputLine;
-			while ((inputLine = br.readLine()) != null) {
-				response.append(inputLine);
+			final DefaultHttpClient httpClient = new DefaultHttpClient();
+			final HttpPost httpPost = new HttpPost(params[2]);
+			final List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(
+					2);
+			nameValuePairs.add(new BasicNameValuePair("j_username", params[0]));
+			nameValuePairs.add(new BasicNameValuePair("j_password", params[1]));
+			httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			httpClient.execute(httpPost);
+			final List<Cookie> cookies = httpClient.getCookieStore()
+					.getCookies();
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("JSESSIONID")) {
+					Registry.register("JSESSIONID", cookie.getValue());
+				}
 			}
-			br.close();
-			connection.disconnect();
-			return response.toString();
+			return ExecutionResult.SUCCESS;
 		} catch (Exception ex) {
 			Log.v(TAG, "Could not send login request", ex);
 		}
-		return null;
+		return ExecutionResult.FAIL;
+	}
+
+	@Override
+	protected void onPostExecute(ExecutionResult result) {
+		switch (result) {
+		case SUCCESS:
+			parent.goToAddBook(null);
+			break;
+		case FAIL:
+			// do something horrible here
+			break;
+		}
 	}
 }
