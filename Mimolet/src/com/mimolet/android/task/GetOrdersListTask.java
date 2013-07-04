68132b414df68003b739daaaf320516985f0ed6b
Boris Petrovskyi <borisp@twodollarweb.com>
2013-07-04 18:57:08 +0300

    Resolve problems with orders list showing.

--- Mimolet/src/com/mimolet/android/task/GetOrdersListTask.java
+++ Mimolet/src/com/mimolet/android/task/GetOrdersListTask.java
@@ -1,5 +1,6 @@
 package com.mimolet.android.task;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -7,12 +8,15 @@ import java.util.Properties;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.mimolet.android.AuthorizationActivity;
-import com.mimolet.android.task.AuthorizationTask.ExecutionResult;
+import com.mimolet.android.util.Registry;
 
 import entity.Order;
 
@@ -30,11 +34,17 @@ public class GetOrdersListTask extends AsyncTask<String, Void, List<Order>> {
   @Override
   protected List<Order> doInBackground(String... params) {
     try {
+      connectionProperties
+      .load(parent.getAssets().open("connection.properties"));
       final DefaultHttpClient httpClient = new DefaultHttpClient();
       final HttpPost httpPost = new HttpPost(connectionProperties.getProperty("server_url") + connectionProperties
           .getProperty("getbyownerid_path"));
+      httpPost.addHeader("Cookie",
+          "JSESSIONID=" + Registry.<String> get("JSESSIONID"));
       HttpResponse responce = httpClient.execute(httpPost);
-      List<Order> list = (ArrayList<Order>) responce.getEntity();
+      Type listType = new TypeToken<ArrayList<Order>>() {
+      }.getType();
+      List<Order> list = new Gson().fromJson(EntityUtils.toString(responce.getEntity()), listType);
       return list;
     } catch (Exception ex) {
       Log.v("GetOrdersList", "Could not get order list", ex);
