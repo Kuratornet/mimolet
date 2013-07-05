ca7508907666f61b46f64b6057b370e0e07dacd5
Boris Petrovskyi <borisp@twodollarweb.com>
2013-07-05 23:09:48 +0300

    Add check Internet connection.

--- Mimolet/src/com/mimolet/android/global/GlobalMethods.java
+++ Mimolet/src/com/mimolet/android/global/GlobalMethods.java
@@ -6,27 +6,41 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
 class GlobalMethod {
-	final private static long DELAY = 60 * 1000 * 10;
-	
-	public static void startTimer (final String url) {
-		final ScheduledExecutorService scheduler = Executors
-				.newScheduledThreadPool(1);
-		scheduler.scheduleWithFixedDelay(new Runnable() {
+  final private static long DELAY = 60 * 1000 * 10;
+  
+  public static void startTimer (final String url) {
+    final ScheduledExecutorService scheduler = Executors
+        .newScheduledThreadPool(1);
+    scheduler.scheduleWithFixedDelay(new Runnable() {
 
-			@Override
-			public void run() {
-				try {
-					final HttpURLConnection connection = (HttpURLConnection) new URL(
-							url + "ping").openConnection();
-					connection.disconnect();
-				} catch (Exception e) {
-					Log.e("ERROR", e.toString());
-				}
-			}
+      @Override
+      public void run() {
+        try {
+          final HttpURLConnection connection = (HttpURLConnection) new URL(
+              url + "ping").openConnection();
+          connection.disconnect();
+        } catch (Exception e) {
+          Log.e("ERROR", e.toString());
+        }
+      }
 
-		}, DELAY, DELAY, TimeUnit.MILLISECONDS);
-	}
+    }, DELAY, DELAY, TimeUnit.MILLISECONDS);
+  }
+  
+  public static boolean isOnline(Activity activity) {
+    ConnectivityManager cm =
+        (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo netInfo = cm.getActiveNetworkInfo();
+    if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+        return true;
+    }
+    return false;
+}
 }
