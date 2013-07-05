736f9b5245abd72ce54fe76db6a221a7e4aab6ce
yshylov <shilov.yuri@gmail.com>
2013-07-06 01:18:12 +0300

    Fix bug with missing id.

--- Mimolet/src/com/mimolet/android/OrdersListActivity.java
+++ Mimolet/src/com/mimolet/android/OrdersListActivity.java
@@ -10,27 +10,27 @@ import android.widget.Toast;
 import com.mimolet.android.adapter.OrderArrayAdapter;
 
 public class OrdersListActivity extends ListActivity {
-  
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_orders_list);
-    
-    Intent intent = getIntent();
-    String[] orders = intent.getStringArrayExtra("orders");
-    String[] images = intent.getStringArrayExtra("imageSources");
-    // in call activity use intent.putExtra("")
-    final ListView listview = (ListView) findViewById(R.id.list);
-    listview.setAdapter(new OrderArrayAdapter(this, R.id.undobar, orders, images));
-  }
 
-  @Override
-  protected void onListItemClick(ListView l, View v, int position, long id) {
- 
-    //get selected items
-    String selectedValue = (String) getListAdapter().getItem(position);
-    Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
- 
-  }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_orders_list);
+
+		Intent intent = getIntent();
+		String[] orders = intent.getStringArrayExtra("orders");
+		String[] images = intent.getStringArrayExtra("imageSources");
+		// in call activity use intent.putExtra("")
+		getListView().setAdapter(
+				new OrderArrayAdapter(this, R.id.undobar, orders, images));
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+
+		// get selected items
+		String selectedValue = (String) getListAdapter().getItem(position);
+		Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
+
+	}
 
 }
