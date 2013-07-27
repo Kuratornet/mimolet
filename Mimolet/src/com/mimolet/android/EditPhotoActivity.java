50e5f9960cab7394a1bf7d072c20a716ee4828dc
yshylov <shilov.yuri@gmail.com>
2013-07-27 22:47:45 +0300

    Add bottom bar buttons switching in EditPhotoActivity.

--- Mimolet/src/com/mimolet/android/EditPhotoActivity.java
+++ Mimolet/src/com/mimolet/android/EditPhotoActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class EditPhotoActivity extends Activity {
 
@@ -11,11 +13,30 @@ public class EditPhotoActivity extends Activity {
 
 	private boolean isLeft;
 
+	private ImageButton[] bottomTabs;
+	private int[] bottomTabsSelectedResources;
+	private int[] bottomTabsNormalResources;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		isLeft = getIntent().getBooleanExtra(IS_LEFT, true);
 		setContentView(R.layout.activity_edit_photo);
+		
+		isLeft = getIntent().getBooleanExtra(IS_LEFT, true);
+
+		bottomTabs = new ImageButton[4];
+		bottomTabs[0] = (ImageButton) findViewById(R.id.chooseLayoutTab);
+		bottomTabs[1] = (ImageButton) findViewById(R.id.chooseBackgroundTab);
+		bottomTabs[2] = (ImageButton) findViewById(R.id.addTextTab);
+		bottomTabs[3] = (ImageButton) findViewById(R.id.changePhotoTab);
+		bottomTabsNormalResources = new int[] { R.drawable.edit_photo_tab1,
+				R.drawable.edit_photo_tab2, R.drawable.edit_photo_tab3,
+				R.drawable.edit_photo_tab4 };
+		bottomTabsSelectedResources = new int[] {
+				R.drawable.edit_photo_tab1_selected,
+				R.drawable.edit_photo_tab2_selected,
+				R.drawable.edit_photo_tab3_selected,
+				R.drawable.edit_photo_tab4_selected };
 	}
 
 	@Override
@@ -23,5 +44,26 @@ public class EditPhotoActivity extends Activity {
 		final MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.edit_photo, menu);
 		return super.onCreateOptionsMenu(menu);
+	}
+
+	private void renderSelectedButton(int selectedButtonId) {
+		for (int i = 0; i < bottomTabs.length; i++) {
+			final ImageButton bottomTab = bottomTabs[i];
+			if (bottomTab.getId() == selectedButtonId) { 
+				bottomTab.setBackgroundResource(bottomTabsSelectedResources[i]);
+			} else {
+				bottomTab.setBackgroundResource(bottomTabsNormalResources[i]);
+			}
+		}
+	}
+
+	public void bottomMenuButtonClick(View view) {
+		switch (view.getId()) {
+		case R.id.chooseLayoutTab:
+		case R.id.chooseBackgroundTab:
+		case R.id.addTextTab:
+		case R.id.changePhotoTab:
+		}
+		renderSelectedButton(view.getId());
 	}
 }
