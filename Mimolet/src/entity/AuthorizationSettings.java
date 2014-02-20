3e046f9f97654d761d7e5e4536f28f74f138fe50
Kuratornet <kuratornet@gmail.com>
2014-02-20 20:37:38 +0200

    Content descriptions to images has been added. Base entities for auto authorization has been added.

---
+++ Mimolet/src/entity/AuthorizationSettings.java
@@ -0,0 +1,39 @@
+package entity;
+
+public class AuthorizationSettings {
+	private AuthorizationType authorizationType = AuthorizationType.UNCHECKED;
+	private String email;
+	private String password;
+	private String facebookValidationID;
+	private String googlePlusValdationID;
+	public AuthorizationType getAuthorizationType() {
+		return authorizationType;
+	}
+	public void setAuthorizationType(AuthorizationType authorizationType) {
+		this.authorizationType = authorizationType;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFacebookValidationID() {
+		return facebookValidationID;
+	}
+	public void setFacebookValidationID(String facebookValidationID) {
+		this.facebookValidationID = facebookValidationID;
+	}
+	public String getGooglePlusValdationID() {
+		return googlePlusValdationID;
+	}
+	public void setGooglePlusValdationID(String googlePlusValdationID) {
+		this.googlePlusValdationID = googlePlusValdationID;
+	}
+}
